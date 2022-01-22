bd1cb8df4316963c9d7e13a27f3cc229bfd2a612
ahnsv <ahnsv@bc.edu>
2022-01-22 20:02:35 +0900

    fix(notion_svc): remove submodule

---
+++ notion_svc/client.ts
@@ -0,0 +1,16 @@
+// TODO: Implement Notion Database Subscriber
+
+export class NotionDatabaseSubscriber {
+    public subscribe(databaseID: string) {
+
+    }
+}
+
+type NotionDatabaseEvent = {
+
+}
+
+
+export class NotionDatabaseEventPublisher {
+    public publish(event: NotionDatabaseEvent) {}
+}
